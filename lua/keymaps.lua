1665f47a049216879926f896d9daf1474fd5c81e
Marcelo de León <mdeleoncosentino@gmail.com>
2022-06-20 13:29:26 -0300

    feat: first keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,3 +11,27 @@ vim.keymap.set("n", "<leader>so", ":source %<cr>")
 vim.keymap.set("n", "<leader>s", ":update<CR>")
 vim.keymap.set("n", "<leader>w", ":q<CR>")
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+
+-- Spellchecking mappings
+vim.keymap.set("n", "<leader>zz", "1z=")
+vim.keymap.set("n", "<leader>zx", "z=")
+
+-- Allow the . to execute once for each line of a visual selection
+vim.keymap.set("v", ".", ":normal .<CR>")
+
+-- Command history filter with <C-n> and <C-p>
+vim.keymap.set("c", "<C-p>", "<Up>")
+vim.keymap.set("c", "<C-n>", "<Down>")
+
+-- Expands path of the active buffer in commmand-line mode when typing '%%'
+-- TODO: Fix, not working
+vim.keymap.set("c", "<expr> %%", "getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
+
+-- Show invisible characters
+vim.keymap.set("n", "<leader>h", ":set list!<CR>")
+
+-- Open quickfix window
+vim.keymap.set("n", "<leader>c", ":copen <CR>")
+
+-- Open locallist window
+vim.keymap.set("n", "<leader>l", ":lopen <CR>")
