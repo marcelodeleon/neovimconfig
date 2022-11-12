7a577a1bd304a4c91c7e6091d31c0e403ee617fd
Marcelo de León <mdeleoncosentino@gmail.com>
2022-11-11 22:43:01 -0300

    chore: add telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,16 @@
 vim.keymap.set("n", "<C-P>", ":lua require('marce.telescope').git_files()<CR>")
--- nnoremap <leader>n :lua require('marce.telescope').edit_neovim()<CR>
--- nnoremap <leader>ps :lua require('telescope.builtin').grep_string()<CR>
--- nnoremap <leader>f :lua require('telescope.builtin').find_files()<CR>
+vim.keymap.set("n", "<leader>n", ":lua require('marce.telescope').edit_neovim()<CR>")
+vim.keymap.set("n", "<leader>ps", ":lua require('telescope.builtin').grep_string()<CR>")
+vim.keymap.set("n", "<leader>f", ":lua require('telescope.builtin').find_files()<CR>")
 
--- nnoremap <leader>pb :lua require('telescope.builtin').buffers()<CR>
--- nnoremap <leader>vh :lua require('telescope.builtin').help_tags()<CR>
--- nnoremap <C-F> :lua require('telescope.builtin').file_browser({ hidden: true })<CR>
--- nnoremap <leader>gb :lua require('marce.telescope').git_branches()<CR>
--- nnoremap <leader>gg :lua require('telescope.builtin').live_grep()<cr>
+vim.keymap.set("n", "<leader>pb", ":lua require('telescope.builtin').buffers()<CR>")
+vim.keymap.set("n", "<leader>vh", ":lua require('telescope.builtin').help_tags()<CR>")
+
+-- TODO: Fix
+vim.keymap.set("n", "<C-F>", ":lua require('telescope.builtin').file_browser({ hidden: true })<CR>")
+vim.keymap.set("n", "<leader>gb", ":lua require('marce.telescope').git_branches()<CR>")
+vim.keymap.set("n", "<leader>gg", ":lua require('telescope.builtin').live_grep()<cr>")
 
 -- " Moody's keymaps
--- nnoremap <leader>j :lua require('marce.telescope').find_maa_ml_monorepo()<CR>
--- nnoremap <leader>gm :lua require('marce.telescope').live_grep_maa_ml_monorepo()<CR>
+vim.keymap.set("n", "<leader>j", ":lua require('marce.telescope').find_maa_ml_monorepo()<CR>")
+vim.keymap.set("n", "<leader>gm", ":lua require('marce.telescope').live_grep_maa_ml_monorepo()<CR>")
