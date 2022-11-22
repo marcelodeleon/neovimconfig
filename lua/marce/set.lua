cbf17a62520a4ce4cea422aebfd1df36b678e71f
Marcelo de León <mdeleoncosentino@gmail.com>
2022-11-22 15:05:04 -0300

    chore: update neovim set config

--- lua/marce/set.lua
+++ lua/marce/set.lua
@@ -5,9 +5,21 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.hlsearch = false
-vim.opt.hlsearch = true
-
+vim.opt.incsearch = true
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
+
+vim.opt.hidden = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.opt.clipboard = 'unnamed'
+
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '88'
+
+vim.opt.undofile = true
+vim.opt.undodir = '~/.vim/undodir'
