7b47812d479e6891f24e197fa01025c6e6f791b7
Marcelo de León <mdeleoncosentino@gmail.com>
2022-12-13 15:38:19 -0300

    chore: update lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,13 +15,14 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', '<space>K', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, opts)
+    vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, opts)
 end
 
 -- this part is telling Neovim to use the lsp server
