b2bdeb6c7708143c2e49f7ae383b7317d73e9e43
Marcelo de Leon <mdeleoncosentino@galileo-ft.com>
2024-04-03 21:50:14 -0300

    chore: configure lspconfig and mason-lspconfig
    There were some changes on how to configure `lsp-zero`,
    specifically regarding lsp servers and cmp settings.
    This change:
      - integrages `lspconfig` and `mason-lspconfig` with `lsp-zero`.
      - fixes the `cmp` mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,47 +2,49 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  'tsserver',
-  'pyright',
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { 'tsserver', 'pyright' },
+  -- Automatically set up servers that are installed
+  automatic_setup = true,
 })
-
--- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
+    
+require("mason-lspconfig").setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    -- ["rust_analyzer"] = function ()
+    --     require("rust-tools").setup {}
+    -- end
+}
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_mappings = {
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
-})
+}
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+cmp.setup({
+    mapping = cmp.mapping.preset.insert(cmp_mappings)
 })
 
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = '',
-        warn = '',
-        hint = '',
-        info = ''
-    }
+lsp.set_sign_icons({
+    error = '',
+    warn = '',
+    hint = '',
+    info = ''
 })
 
 lsp.on_attach(function(client, bufnr)
