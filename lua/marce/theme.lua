4401488ef694290a7fb7711886b4b2cbb111dd84
Marcelo de Leon <mdeleoncosentino@gmail.com>
2024-05-15 18:05:25 -0300

    fix(dracula): hack for markdown syntax errors for treesitter
    This is not a definitive solution, should check the dracula pro team
    response and hopefully add a newer version.
    Discord convo here:
    https://discord.com/channels/820858204472213505/849515983295217675/1240397953793921046

--- lua/marce/theme.lua
+++ lua/marce/theme.lua
@@ -7,3 +7,33 @@ vim.cmd [[packadd! dracula_pro]]
 vim.cmd [[syntax enable]]
 vim.g.dracula_colorterm = 0
 vim.cmd [[colorscheme dracula_pro]]
+
+
+-- This is a Hack to add some colors to markdown when using treesitter and
+-- dracula pro. The pro theme is missing the highlight groups for markdown...
+-- I'm waiting for the team to let me know how to get the fixed version. I'll
+-- keep this in the meantime.
+local function set_markdown_highlights()
+    -- Base colors from Dracula Theme
+    vim.api.nvim_set_hl(0, 'Comment', {fg = '#6272a4', italic = true})
+    vim.api.nvim_set_hl(0, 'Constant', {fg = '#ffb86c'})
+    vim.api.nvim_set_hl(0, 'Identifier', {fg = '#50fa7b'})
+    vim.api.nvim_set_hl(0, 'Statement', {fg = '#ff79c6', bold = true})
+    vim.api.nvim_set_hl(0, 'PreProc', {fg = '#ff79c6'})
+    vim.api.nvim_set_hl(0, 'Type', {fg = '#8be9fd'})
+    vim.api.nvim_set_hl(0, 'Underlined', {fg = '#f1fa8c', underline = true})
+    
+    -- Markdown specific highlights
+    vim.api.nvim_set_hl(0, '@markup.heading.1', {fg = '#ff79c6', bold = true})
+    vim.api.nvim_set_hl(0, '@markup.heading.2', {fg = '#8be9fd', bold = true})
+    vim.api.nvim_set_hl(0, '@markup.heading.3', {fg = '#50fa7b', bold = true})
+    vim.api.nvim_set_hl(0, '@markup.heading.4', {fg = '#ffb86c', bold = true})
+    vim.api.nvim_set_hl(0, '@markup.heading.5', {fg = '#bd93f9', bold = true})
+    vim.api.nvim_set_hl(0, '@markup.list', {fg = '#f1fa8c'})
+end
+
+-- Automatically set custom highlights when entering a Markdown buffer
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = set_markdown_highlights
+})
