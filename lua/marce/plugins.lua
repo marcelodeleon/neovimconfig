e75cca39c0ff762f3017a27f457a42cd5efb121b
Marcelo de Leon <mdeleoncosentino@gmail.com>
2024-06-12 17:52:04 -0300

    fix(lazy): fix lazy bootsrap code issue

--- lua/marce/plugins.lua
+++ lua/marce/plugins.lua
@@ -2,8 +2,6 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
-        c
-
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
